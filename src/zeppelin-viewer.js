fad452a3a39b1b2689ff805535eb4a5b560b3e06
sravan <sravan.s@flytxt.com>
2016-07-29 12:09:44 +0530

    Initial commit

---
+++ src/zeppelin-viewer.js
@@ -0,0 +1,14 @@
+Polymer({
+
+  is: 'zeppelin-viewer',
+
+  properties: {
+
+    //URL for zeppelin
+    zeppelinUrl: {
+      type: String,
+      value: 'http://localhost:8080/api/notebook'
+    }
+  },
+
+});
