1cc916997e03eb083113d097f6276c2992995482
sravan <sravan.s@flytxt.com>
2016-07-29 16:28:01 +0530

    Basix vulcanize implemented

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,30 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var vulcanize = require('gulp-vulcanize');
+
+var CONSTS = {
+  // Set Zeppelin URL here
+  zeppelinUrl: 'http://localhost:8080'
+}
+
+// Vulcanize
+gulp.task('vulcanize', function () {
+    return gulp.src('src/zeppelin-viewer.html')
+      .pipe(vulcanize({
+          abspath: '',
+          excludes: [],
+          stripExcludes: false
+      }))
+      .pipe(gulp.dest('dist'));
+});
 
 // Static server
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "",
-            directory: true
-        }
-    });
+  browserSync.init({
+      server: {
+        baseDir: '',
+        directory: true
+      },
+  });
+  gulp.watch('src/\*\*/\*.*').on('change', browserSync.reload);
 });
-
-// // or...
-
-// gulp.task('browser-sync', function() {
-//     browserSync.init({
-//         proxy: "yourlocal.dev"
-//     });
-// });
