2c51b11c877727a5af47111a098e993aa13da8cf
Arun Soman <aarunsoman@gmail.com>
2016-08-22 10:33:27 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,19 @@ gulp.task('browser-sync', function() {
     });
 });
 
-// // or...
+gulp.task('eslint', function () {
+    return gulp.src([
+'src/*'
 
-// gulp.task('browser-sync', function() {
-//     browserSync.init({
-//         proxy: "yourlocal.dev"
-//     });
-// });
+                     ])
+        // eslint() attaches the lint output to the eslint property 
+        // of the file object so it can be used by other modules. 
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console. 
+        // Alternatively use eslint.formatEach() (see Docs). 
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on 
+        // lint error, return the stream and pipe to failOnError last. 
+        .pipe(eslint.failOnError());
+});
+gulp.task('default', ['eslint']);
